f8ff46b0375209a0e87b811e8a5a1e01c67a5d86
Vladislav Nechepaev <vladislav.nechepaev@swedbyte.se>
2022-05-12 15:43:52 +0300

    12.05.2022 wfo

--- ringteam_wfo.js
+++ ringteam_wfo.js
@@ -40,6 +40,7 @@ jQuery(document).on('ready', async function(){
   const today = new Date()
   const todayGlobal = today.toLocaleDateString()
   const [thisWeekStart, nextWeekStart] = getWeekStarts()
+  console.log(thisWeekStart, nextWeekStart)
   var selectedDatesGlobal = []
   var capacityCache
   var capacityCheckMain = true
