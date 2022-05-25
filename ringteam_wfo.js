7a8646cbf9a28d704460b11aa73355eae0fb1f50
Vladislav Nechepaev <vladislav.nechepaev@swedbyte.se>
2022-05-25 16:55:22 +0300

    25.05

--- ringteam_wfo.js
+++ ringteam_wfo.js
@@ -409,7 +409,8 @@ ${parkingActive ? "&getparking=true" : ""}`)
         const selectOptions = document.getElementsByClassName("parking-option-" + optionName)
         for (let elem of selectOptions) {
           elem.style.display = parkingSettings[optionName].officeList.includes(values.office) ? "block" : "none"
-          if (optionName === "parking_platforma_test") {
+          if (optionName.startsWith("parking_platforma")) {
+            console.log("personal id:", personalParkingSpotId)
             if (personalParkingSpotId) {
               elem.innerHTML = parkingSettings[optionName].label + ", spot #" + personalParkingSpotId
             } else {
